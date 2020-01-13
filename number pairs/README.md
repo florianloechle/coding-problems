d3312f2ca4551cf0abbab8f0d51138d78e8707ef
Löchle <florian.loechle@bechtle.com>
2020-01-13 11:17:18 +0100

    Add coding problem: Number Pairs

---
+++ number pairs/README.md
@@ -0,0 +1,8 @@
+# Number Pairs
+
+Given a *list of numbers* and a number *k*, return whether any two numbers from the list add up to *k*.
+For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+This coding problem was sent to me by [Daily Coding Problem](https://dailycodingproblem.com).
+
+
---
+++ number pairs/index.js
@@ -0,0 +1,44 @@
+/**
+ *
+ * @param {Array} list
+ * @param {Number} k
+ */
+function numberPairExists(list, k) {
+  if (!Array.isArray(list) || list.length === 0) {
+    return false;
+  }
+  const length = list.length;
+  for (let i = 0; i<=length; i++) {
+    const value = list[i];
+    for (let j = 0; j <= length; j++) {
+      if (j !== i && value + list[j] === k) {
+        return true;
+      }
+    }
+  }
+  return false
+}
+
+/**
+ *
+ * @param {Array} list
+ * @param {Number} k
+ */
+function numberPairExistsWithSet(list,k) {
+  if (!Array.isArray(list) || list.length === 0) {
+    return false;
+  }
+  const numbers = new Set(list);
+  for(let i = 0; i<=list.length;i++) {
+    const rest = k - list[i];
+    if (numbers.has(rest)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+module.exports = {
+  numberPairExists,
+  numberPairExistsWithSet
+}
